4e899ca412a2086bcf5a0021be20281ba01e39d4
SpongeBob90 <1442772080@qq.com>
2018-03-09 16:35:02 +0800

    B提交代码

--- src/main/java/com/wyw/controller/SourceTreeTestController.java
+++ src/main/java/com/wyw/controller/SourceTreeTestController.java
@@ -1,8 +1,20 @@
 package com.wyw.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 /**
  * @author wyw
  * @date 2018\3\9 0009 16:27
  */
+@Controller
 public class SourceTreeTestController {
+
+    @GetMapping(value = "/st1")
+    @ResponseBody
+    public String hello1(){
+        return "Hello SourceTree1 !";
+    }
+
 }
